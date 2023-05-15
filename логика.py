755a9a2d70c4bb8f6e0b8bdb8512b901fa159c81
goshaobolonskiy <goshaobolonskiy@mail.ru>
2023-05-15 20:49:25 +0300

    new

---
+++ логика.py
@@ -0,0 +1,43 @@
+import time
+class Prioritet:
+    count = 0
+    list = []
+    def __init__(self, name, priority, urgency):
+        self.name = name
+        self.priority = priority
+        self.urgency = urgency
+        Prioritet.count += 1
+        self.list.append(self)
+    def __str__(self):
+        return f"{self.name = nameself.priority, self.urgency}"
+
+
+def new(name, priority, urgency):
+    a = Prioritet(name, priority, urgency)
+
+
+
+# a = Prioritet(1, 1, 2)
+new("Домашнее задание", 2, 2)
+new("Работа", 1, 7)
+
+Prioritet.list = sorted(Prioritet.list, key=lambda x: x.priority)
+print(Prioritet.list)
+for i in range(Prioritet.count):
+    print(Prioritet.list[i])
+
+
+
+# t = []
+# time1 = time.localtime(time.time())
+# print(time.localtime(time.time()))
+# # for i in range(0,8):
+# #     t.append(time1[i])
+# # print(t)
+# time2 = time.mktime(time1)
+# print(time2)
+# print(prioritet.__dict__)
+
+
+# Перетаскивание задачи
+# Определение прогой приоритетов
